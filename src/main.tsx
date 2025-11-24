586fdef3ee487f2840cd734110204dd2141ab47f
André Luis Mari <andreluismari@hotmail.com>
2025-11-24 16:46:57 -0300

    Atualizando código

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
